5ebe7b005b94cb3b9b5c47f947fd102898104a19
Andy Odle <andy.odle@blacklightcoders.com>
2015-03-12 22:57:33 -0700

    CBGGS: - Added initial get hotkey from user functionality.

---
+++ HotkeyManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatboobGGStream
+{
+    public class HotkeyManager
+    {
+        private String keys_string;
+        private List<String> pressed_keys;
+
+        public HotkeyManager()
+        {
+            pressed_keys = new List<String>();
+        }
+
+        public String GetPressedKeysString()
+        {
+            keys_string = "";
+
+            for (int count = 0; count < pressed_keys.Count; count++)
+            {
+                String tempKey = pressed_keys[count];
+
+                if (count > 0 && count < pressed_keys.Count)
+                    keys_string = String.Format("{0} + {1}", keys_string, tempKey);
+                else
+                    keys_string += tempKey;
+            }
+
+            return keys_string;
+        }
+
+
+        public bool RemovePressedKey(String key_to_remove)
+        {
+            for (int count = 0; count < pressed_keys.Count; count++)
+            {
+                if (pressed_keys[count] == key_to_remove)
+                {
+                    pressed_keys.Remove(key_to_remove);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool AddPressedKey(String key_to_add)
+        {
+            if (!pressed_keys.Contains(key_to_add))
+            {
+                pressed_keys.Add(key_to_add);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Shapes;
 using System.ComponentModel;
 using System.Drawing;
 using System.IO;
+using Microsoft.Win32;
 
 namespace CatboobGGStream
 {
@@ -24,8 +25,11 @@ namespace CatboobGGStream
     /// </summary>
     public partial class MainWindow : Window
     {
+        private String pressed_hotkey;
         private String working_dir;
+        private HotkeyManager hotkey_manager;
         private BindingList<OverlayItem> overlay_items;
+
         public BindingList<OverlayItem> OverlayItems 
         { 
             get{return overlay_items;} 
@@ -43,6 +47,8 @@ namespace CatboobGGStream
 
             // Get Applicaion Directory
             working_dir = Directory.GetCurrentDirectory();
+
+            hotkey_manager = new HotkeyManager();
 
             //TODO: Replace with config file.
             OverlayItem temp_item;
@@ -95,16 +101,37 @@ namespace CatboobGGStream
             // Show the add item container.
             add_overlay_item_container.Visibility = System.Windows.Visibility.Visible;
 
-            // Show the back navigation button.
+            // Show the cancel button.
             Cancel_btn.Visibility = System.Windows.Visibility.Visible;
 
             // Show the save item button.
             Save_btn.Visibility = System.Windows.Visibility.Visible;
+
+            // Hide the hotkey diallog.
+            hotkey_dialog.Visibility = System.Windows.Visibility.Collapsed;
+        }
+
+        private void DisplayHotkeyDialog()
+        {
+            // Show the hotkey dialog.
+            hotkey_dialog.Visibility = System.Windows.Visibility.Visible;
+
+            hotkey_manager = new HotkeyManager();
+            pressed_key_tb.Text = "";
+            pressed_key_tb.Focus();
+        }
+
+        private void ResetAddItemForm()
+        {
+            // Reset add item form.
+            hotkey_tb.Text = "";
+            image_path_tb.Text = "";
         }
 
         private void AddItem_Click(object sender, RoutedEventArgs e)
         {
             DisplayAddItemScreen();
+            ResetAddItemForm();
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
@@ -119,15 +146,68 @@ namespace CatboobGGStream
 
         private void SaveButton_Click(object sender, RoutedEventArgs e)
         {
-            // Testing Items
-            OverlayItem temp_item;
+            OverlayItem temp_item = new OverlayItem();
 
-            temp_item = new OverlayItem();
-            temp_item.HotKey = "CTRL + F5";
-            temp_item.ImagePath = working_dir + "\\Images\\100_0105.jpg";
+            // Get the hotkey to assign.
+            if (!String.IsNullOrEmpty(hotkey_tb.Text))
+                temp_item.HotKey = hotkey_tb.Text;
+            
+            // Get the selected image path.
+            if(!String.IsNullOrEmpty(image_path_tb.Text))
+                temp_item.ImagePath = image_path_tb.Text;
+
             OverlayItems.Add(temp_item);
 
             DisplayStartingScreen();
+        }
+
+        private void ImagePath_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog open_file_dialog = new OpenFileDialog();
+            if (open_file_dialog.ShowDialog() == true)
+                image_path_tb.Text = open_file_dialog.FileName;
+        }
+
+        private void HotKey_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayHotkeyDialog();
+        }
+
+        private void HotkeyDiscard_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayAddItemScreen();
+        }
+
+        private void HotkeySave_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayAddItemScreen();
+
+            // Save the enterted hotkey.
+            String tempHotkey = pressed_key_tb.Text;
+            if(!String.IsNullOrEmpty(tempHotkey))
+                hotkey_tb.Text = tempHotkey;
+        }
+
+        private void Hotkey_KeyUp(object sender, KeyEventArgs e)
+        {
+            // Remove key from hotkey manager.
+            hotkey_manager.RemovePressedKey(e.Key.ToString());                
+        }
+
+        private void Hotkey_Down(object sender, KeyEventArgs e)
+        {
+            // Add pressed keys to the hotkey manager.
+            hotkey_manager.AddPressedKey(e.Key.ToString());
+
+            // Display the list of key in a hotkey.
+            pressed_key_tb.Text = hotkey_manager.GetPressedKeysString();
+        }
+
+        private void Hotkey_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            // Ignore repeated keys.
+            if (e.IsRepeat)
+                e.Handled = true;
         }
     }
 }
